0218eb7b4892f27bf6e349b4738f53d3975554dc
dgrin1 <dgrin@haverford.edu>
2022-08-20 20:09:19 -0400

    Delete MakingShapes.py

--- MakingShapes.py
+++
@@ -1,7 +0,0 @@
-from vpython import *
-#Web VPython 3.2
-print("Hello,world!")
-#sphere(position=vector(0,0,0),radius=5,color=color.blue)
-box(position=vector(0,0,0),size=vector(0.5,0.3,0.2),color=color.red)
-box(position=vector(2,0,0),size=vector(0.5,0.3,0.2),color=color.blue)
-box(position=vector(0,2,0),size=vector(0.5,0.3,0.2),color=color.green)
